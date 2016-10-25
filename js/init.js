d9e2db8ad9007ac978c1b165e07e6fa13f0028c7
Tumble <camtrow@sky.com>
2016-10-25 17:25:41 +0100

    carasel

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({full_width: true});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
