acfc35fae232ff956a1fa90b091ec1e53078ffc2
Tumble <camtrow@sky.com>
2016-10-31 22:43:01 +0000

    services

--- js/init.js
+++ js/init.js
@@ -1,12 +1,23 @@
 (function($){
   $(function(){
 
+//PRELOADER
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
+//PRELOADER end
+
+
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('ul.tabs').tabs();
     $('.slider').slider({full_width: true});
     $('.scrollspy').scrollSpy();
     $(".dropdown-button").dropdown();
+
 
 
   }); // end of document ready
